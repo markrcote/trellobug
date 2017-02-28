ff4e4ed15dd57977c531482f7fed45e7881236e9
Mark Côté <mcote@alumni.uwaterloo.ca>
2017-02-27 20:36:42 -0500

    Refactor most code into a class.
    The `config` variable and a couple others were being passed around too much,
    so the functions have been reorganized into a class.  The Trello-token
    error handling was also refactored into a decorator.

--- trellobug.py
+++ trellobug.py
@@ -48,166 +48,210 @@ def get_bugzilla_error(e):
     return 'Error {}: {}'.format(error_dict['code'], error_dict['message'])
 
 
-def query_option(config, section, option, desc, instructions):
-    if option not in config[section]:
-        val = None
-        print('{} not found.'.format(desc))
-        print('\n'.join(textwrap.wrap(instructions)))
+def check_trello_tokens(func):
+    """Decorator to handle expired Trello OAuth tokens.
 
-        while not val:
-            print()
-            print('\n'.join(textwrap.wrap(
-                'You can enter one here, or use ctrl-C to quit and add it '
-                'manually to your config file as "[{}]{}":'.format(
-                    section, option)
-            )))
-            val = input()
+    Catches `Unauthorized` errors from trello and calls
+    self.handle_expired_trello_tokens().  It will retry forever until
+    successful.
+    """
+    def func_wrapper(*args, **kwargs):
+        while True:
+            try:
+                func(*args, **kwargs)
+                return
+            except Unauthorized:
+                print('Unauthorized!')
+                args[0].handle_expired_trello_tokens()
+    return func_wrapper
 
-        config.set(section, option, val)
+
+class TrelloBug(object):
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.config = None
+        self.bz_config = None
+        self.trello_config = None
+        self.load_config()
+        self.load_trello()
+
+    @check_trello_tokens
+    def trello_to_bug(self, card_id):
+        card = self.trello.get_card(card_id)
+
+        bug = self.file_trello_bug(card)
+
+        if not bug:
+            return False
+
+        print('Bug {} <{}> filed:'.format(bug['id'], bug['url']))
+        print('    {}'.format(bug['summary']))
+
+        card.set_description('{}\n\n{}'.format(bug['url'],
+                                               card.description))
+
+        print ('Card {} updated.'.format(card.short_url))
         return True
 
-    return False
+    def query_option(self, section, option, desc, instructions):
+        if option not in self.config[section]:
+            val = None
+            print('{} not found.'.format(desc))
+            print('\n'.join(textwrap.wrap(instructions)))
 
+            while not val:
+                print()
+                print('\n'.join(textwrap.wrap(
+                    'You can enter one here, or use ctrl-C to quit and add it '
+                    'manually to your config file as "[{}]{}":'.format(
+                        section, option)
+                )))
+                val = input()
 
-def generate_trello_oauth_tokens(config):
-    print('Press enter to generate.')
-    input()
+            self.config.set(section, option, val)
+            return True
 
-    access_token = create_oauth_token(
-        expiration='30days',
-        key=config['trello']['api_key'],
-        secret=config['trello']['api_secret'],
-        name='trello-to-bug',
-        output=False,
-    )
+        return False
 
-    for opt in ('oauth_token', 'oauth_token_secret'):
-        config.set('trello', opt, access_token[opt])
+    def generate_trello_oauth_tokens(self):
+        print('Press enter to generate.')
+        input()
 
-    print('\n'.join(textwrap.wrap(
-        'Token generated.  It will expire in 30 days, after which this '
-        'script will generate a new one.')))
+        access_token = create_oauth_token(
+            expiration='30days',
+            key=self.trello_config['api_key'],
+            secret=self.trello_config['api_secret'],
+            name='trello-to-bug',
+            output=False,
+        )
 
+        for opt in ('oauth_token', 'oauth_token_secret'):
+            self.trello_config[opt] = access_token[opt]
 
-def get_trello(config):
-    return TrelloClient(
-        api_key=config['trello']['api_key'],
-        api_secret=config['trello']['api_secret'],
-        token=config['trello']['oauth_token'],
-        token_secret=config['trello']['oauth_token_secret']
-    )
+        print('\n'.join(textwrap.wrap(
+            'Token generated.  It will expire in 30 days, after which this '
+            'script will generate a new one.')))
 
+    def load_trello(self):
+        self.trello = TrelloClient(
+            api_key=self.trello_config['api_key'],
+            api_secret=self.trello_config['api_secret'],
+            token=self.trello_config['oauth_token'],
+            token_secret=self.trello_config['oauth_token_secret']
+        )
 
-def handle_expired_trello_tokens(config, config_file):
-    print('Trello OAuth token invalid or expired.')
-    generate_trello_oauth_tokens(config)
-    write_config(config, config_file)
-    return get_trello(config)
+    def handle_expired_trello_tokens(self):
+        print('Trello OAuth token invalid or expired.')
+        self.generate_trello_oauth_tokens()
+        self.write_config()
+        self.load_trello()
 
+    def file_trello_bug(self, card):
+        card_name = card.name
+        m = story_name_with_points.match(card_name)
 
-def file_trello_bug(bz_config, card):
-    card_name = card.name
-    m = story_name_with_points.match(card_name)
+        if m:
+            card_name = m.group(1)
 
-    if m:
-        card_name = m.group(1)
+        bugzilla_url_base = self.bz_config.get(
+            'url', DEFAULT_BUGZILLA_URL).rstrip('/')
 
-    bugzilla_url_base = bz_config.get('url', DEFAULT_BUGZILLA_URL).rstrip('/')
+        url = bug_api_url_tmpl.format(bugzilla_url_base)
 
-    url = bug_api_url_tmpl.format(bugzilla_url_base)
+        bug_data = {
+            'api_key': self.bz_config['api_key'],
+            'product': self.bz_config.get('product', DEFAULT_PRODUCT),
+            'component': self.bz_config.get('component', DEFAULT_COMPONENT),
+            'version': self.bz_config.get('version', DEFAULT_VERSION),
+            'summary': card_name,
+            'description': card.description,
+            'url': card.short_url,
+            'op_sys': 'Unspecified',
+            'platform': 'Unspecified',
+        }
 
-    bug_data = {
-        'api_key': bz_config['api_key'],
-        'product': bz_config.get('product', DEFAULT_PRODUCT),
-        'component': bz_config.get('component', DEFAULT_COMPONENT),
-        'version': bz_config.get('version', DEFAULT_VERSION),
-        'summary': card_name,
-        'description': card.description,
-        'url': card.short_url,
-        'op_sys': 'Unspecified',
-        'platform': 'Unspecified',
-    }
+        headers = {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        }
 
-    headers = {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-    }
+        request = Request(
+            url=url,
+            data=json.dumps(bug_data).encode('utf8'),
+            headers=headers,
+            method='POST',
+        )
 
-    request = Request(
-        url=url,
-        data=json.dumps(bug_data).encode('utf8'),
-        headers=headers,
-        method='POST',
-    )
+        try:
+            with urlopen(request) as f:
+                response = json.loads(f.read().decode('utf8'))
+        except HTTPError as e:
+            error = get_bugzilla_error(e)
+            print('Error sending request to Bugzilla: {}'.format(error))
+            return None
 
-    try:
-        with urlopen(request) as f:
-            response = json.loads(f.read().decode('utf8'))
-    except HTTPError as e:
-        error = get_bugzilla_error(e)
-        print('Error sending request to Bugzilla: {}'.format(error))
-        return 1
+        bug = {
+            'id': response['id'],
+            'url': bug_url_tmpl.format(bugzilla_url_base, response['id']),
+            'summary': card_name,
+        }
 
-    bug = {
-        'id': response['id'],
-        'url': bug_url_tmpl.format(bugzilla_url_base, response['id']),
-        'summary': card_name,
-    }
+        return bug
 
-    return bug
+    def write_config(self):
+        print('Saving changes to {}.'.format(self.config_file))
 
+        with open(self.config_file, 'w') as f:
+            self.config.write(f)
 
-def write_config(config, config_file):
-    print('Saving changes to {}.'.format(config_file))
+        print()
 
-    with open(config_file, 'w') as f:
-        config.write(f)
+    def load_config(self):
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_file)
 
-    print()
+        if 'bugzilla' not in self.config:
+            self.config.add_section('bugzilla')
 
+        if 'trello' not in self.config:
+            self.config.add_section('trello')
 
-def load_config(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
+        self.bz_config = self.config['bugzilla']
+        self.trello_config = self.config['trello']
 
-    if 'bugzilla' not in config:
-        config.add_section('bugzilla')
+        if 'url' not in self.bz_config:
+            print('Using the Bugzilla instance at {}'.format(
+                DEFAULT_BUGZILLA_URL))
 
-    if 'trello' not in config:
-        config.add_section('trello')
+        changed = False
 
-    if 'url' not in config['bugzilla']:
-        print('Using the Bugzilla instance at {}'.format(DEFAULT_BUGZILLA_URL))
+        changed |= self.query_option(
+            'bugzilla', 'api_key', 'Bugzilla API key',
+            'Please visit '
+            'https://bugzilla.mozilla.org/userprefs.cgi?tab=apikey '
+            'to see your existing API keys or to generate a new one.'
+        )
 
-    changed = False
+        changed |= self.query_option(
+            'trello', 'api_key', 'Trello API key',
+            'You can see your API key at '
+            'https://trello.com/1/appKey/generate in the top box.'
+        )
 
-    changed |= query_option(
-        config, 'bugzilla', 'api_key', 'Bugzilla API key',
-        'Please visit https://bugzilla.mozilla.org/userprefs.cgi?tab=apikey '
-        'to see your existing API keys or to generate a new one.'
-    )
+        changed |= self.query_option(
+            'trello', 'api_secret', 'Trello API secret',
+            'You can see your API secret at https://trello.com/app-key at the '
+            'bottom under "OAuth".'
+        )
 
-    changed |= query_option(
-        config, 'trello', 'api_key', 'Trello API key',
-        'You can see your API key at https://trello.com/1/appKey/generate in '
-        'the top box.'
-    )
+        if ('oauth_token' not in self.trello_config or
+                'oauth_token_secret' not in self.trello_config):
+            self.generate_trello_oauth_tokens()
+            changed = True
 
-    changed |= query_option(
-        config, 'trello', 'api_secret', 'Trello API secret',
-        'You can see your API secret at https://trello.com/app-key at the '
-        'bottom under "OAuth".'
-    )
-
-    if ('oauth_token' not in config['trello'] or
-            'oauth_token_secret' not in config['trello']):
-        generate_trello_oauth_tokens(config)
-        changed = True
-
-    if changed:
-        write_config(config, config_file)
-
-    return config
+        if changed:
+            self.write_config()
 
 
 def main(config_file, card_id):
@@ -218,34 +262,9 @@ def main(config_file, card_id):
         else:
             config_file = DEFAULT_CONFIG_FILES[0]
 
-    config = load_config(config_file)
-
-    bz_config = config['bugzilla']
-
-    trello = get_trello(config)
-    card = None
-
-    while not card:
-        try:
-            card = trello.get_card(card_id)
-        except Unauthorized:
-            trello = handle_expired_trello_tokens(config, config_file)
-
-    bug = file_trello_bug(bz_config, card)
-
-    print('Bug {} <{}> filed:'.format(bug['id'], bug['url']))
-    print('    {}'.format(bug['summary']))
-
-    while True:
-        try:
-            card.set_description('{}\n\n{}'.format(bug['url'],
-                                                   card.description))
-            break
-        except Unauthorized:
-            trello = handle_expired_trello_tokens(config, config_file)
-
-    print ('Card {} updated.'.format(card.short_url))
-    return 0
+    trello_to_bug = TrelloBug(config_file)
+    success = trello_to_bug.trello_to_bug(card_id)
+    return 0 if success else 1
 
 
 if __name__ == '__main__':
